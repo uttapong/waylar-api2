e8219355a6ba842a594731302c1119b0eb697d64
Uttapong Ruangrit <uttapong@gmail.com>
2018-05-03 19:45:51 +0700

    add authen

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(require('./authen'));
+
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
@@ -8,3 +10,5 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Start server at port 3000.')
 })
+
+
