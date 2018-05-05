3ae1cd9c3c617c4081d4b2c0df1741508b275d0c
Uttapong Ruangrit <uttapong@gmail.com>
2018-05-05 18:07:36 +0700

    add create users api

--- authen.js
+++ authen.js
@@ -11,34 +11,51 @@ router.use(function timeLog(req, res, next) {
 //register
 router.post('/user', function(req, res) {
 
+    // var options = { method: 'POST',
+    // url: 'https://tnk.auth0.com/oauth/token',
+    // headers: { 'content-type': 'application/json' },
+    // body: '{"client_id":"DEgjSOxq9pBJJIv6hbuIUDAYf0QwjIV5","client_secret":"5mtNz2NxBf-jIPABvMZV14j7FIEtqD3p9lYJ-YaWkv-lJqSptONtLLSArkERoZ7i","audience":"https://tnk.auth0.com/api/v2/","grant_type":"client_credentials","json":"true"' };
+
     var options = { method: 'POST',
-    url: 'https://tnk.auth0.com/oauth/token',
-    headers: { 'content-type': 'application/json' },
-    body: '{"client_id":"DEgjSOxq9pBJJIv6hbuIUDAYf0QwjIV5","client_secret":"5mtNz2NxBf-jIPABvMZV14j7FIEtqD3p9lYJ-YaWkv-lJqSptONtLLSArkERoZ7i","audience":"https://tnk.auth0.com/api/v2/","grant_type":"client_credentials"}' };
+  url: 'https://tnk.auth0.com/oauth/token',
+  headers: { 'content-type': 'application/json' },
+  body: 
+   { grant_type: 'client_credentials',
+     client_id: 'DEgjSOxq9pBJJIv6hbuIUDAYf0QwjIV5',
+     client_secret: '5mtNz2NxBf-jIPABvMZV14j7FIEtqD3p9lYJ-YaWkv-lJqSptONtLLSArkERoZ7i',
+     audience: 'https://tnk.auth0.com/api/v2/' },
+  json: true };
 
     request(options, function (error, response, body) {
     if (error) throw new Error(error);
 
-        //res.send(body);
+        // res.send(JSON.parse(body).access_token);
+        // res.send(body.access_token);
         var form_data= {
-            "client_id": "DEgjSOxq9pBJJIv6hbuIUDAYf0QwjIV5",
-            "email": "adadf@afda.com",
-            "password": "adfadfadfadf",
+            "user_id": "",
             "connection": "Username-Password-Authentication",
-            "user_metadata": { plan: 'silver', team_id: 'a111' }
-          };
+            "email": "john.doe@gmail.com",
+            "password": "secret"
+          }
 
-        var options = { method: 'GET',
-        url: 'https://tnk.auth0.com/dbconnections/signup',
-        headers: { authorization: 'Bearer '+body.access_token }, 
-        form_data:form_data};
+        var options = { method: 'POST',
+        url: 'https://tnk.auth0.com/api/v2/users',
+        headers: { 'content-type': 'Content-Type: application/json',authorization: 'Bearer '+body.access_token } ,
+        form:form_data};
         
-       
+        
         request(options, function (error, response, body) {
             if (error) throw new Error(error);
         
             res.send(body);
         });
+
+//         request.post({url:'https://tnk.auth0.com/api/v2/users', formData: form_data}, function optionalCallback(err, httpResponse, body) {
+//   if (err) {
+//     return console.error('upload failed:', err);
+//   }
+//   res.send(body);
+// });
     });
 });
 
