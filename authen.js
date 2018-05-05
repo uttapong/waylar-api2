c41a8f7e0731b3bc5a5c67b93e0894c50b4d9461
Uttapong Ruangrit <uttapong@gmail.com>
2018-05-05 18:37:48 +0700

    add lib

--- authen.js
+++ authen.js
@@ -1,6 +1,8 @@
+require('babel-register');
 var express = require('express');
 var router = express.Router();
 var request = require("request");
+import {getToken} from './lib'
 //Middle ware that is specific to this router
 router.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
@@ -9,54 +11,38 @@ router.use(function timeLog(req, res, next) {
 
 
 //register
-router.post('/user', function(req, res) {
+router.post('/user', (req, res)=> {
 
     // var options = { method: 'POST',
     // url: 'https://tnk.auth0.com/oauth/token',
     // headers: { 'content-type': 'application/json' },
     // body: '{"client_id":"DEgjSOxq9pBJJIv6hbuIUDAYf0QwjIV5","client_secret":"5mtNz2NxBf-jIPABvMZV14j7FIEtqD3p9lYJ-YaWkv-lJqSptONtLLSArkERoZ7i","audience":"https://tnk.auth0.com/api/v2/","grant_type":"client_credentials","json":"true"' };
 
-    var options = { method: 'POST',
-  url: 'https://tnk.auth0.com/oauth/token',
-  headers: { 'content-type': 'application/json' },
-  body: 
-   { grant_type: 'client_credentials',
-     client_id: 'DEgjSOxq9pBJJIv6hbuIUDAYf0QwjIV5',
-     client_secret: '5mtNz2NxBf-jIPABvMZV14j7FIEtqD3p9lYJ-YaWkv-lJqSptONtLLSArkERoZ7i',
-     audience: 'https://tnk.auth0.com/api/v2/' },
-  json: true };
+    
+    
 
-    request(options, function (error, response, body) {
-    if (error) throw new Error(error);
+   getToken.then((token)=>{
+    // res.send(token);
+    let form_data= {
+      "user_id": "",
+      "connection": "Username-Password-Authentication",
+      "email": "john.doe@gmail.com",
+      "password": "secret"
+    }
 
-        // res.send(JSON.parse(body).access_token);
-        // res.send(body.access_token);
-        var form_data= {
-            "user_id": "",
-            "connection": "Username-Password-Authentication",
-            "email": "john.doe@gmail.com",
-            "password": "secret"
-          }
-
-        var options = { method: 'POST',
+    let options = { method: 'POST',
         url: 'https://tnk.auth0.com/api/v2/users',
-        headers: { 'content-type': 'Content-Type: application/json',authorization: 'Bearer '+body.access_token } ,
+        headers: { 'content-type': 'Content-Type: application/json',authorization: 'Bearer '+token } ,
         form:form_data};
-        
-        
-        request(options, function (error, response, body) {
-            if (error) throw new Error(error);
-        
-            res.send(body);
-        });
-
-//         request.post({url:'https://tnk.auth0.com/api/v2/users', formData: form_data}, function optionalCallback(err, httpResponse, body) {
-//   if (err) {
-//     return console.error('upload failed:', err);
-//   }
-//   res.send(body);
-// });
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+  
+      res.send(body);
     });
+   }).catch((error)=>{
+    res.send(error);
+   })
+
 });
 
 //get user info
